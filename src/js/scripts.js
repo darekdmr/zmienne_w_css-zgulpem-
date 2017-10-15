74ac959e21d4b7523dec111f8508204a13f0bc3c
Dariusz Mierzejewski <dariusz.mierzejewski@gmail.com>
2017-10-15 21:55:34 +0200

    prawie skończone - działa w 50% :)

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,15 @@
 // scripts.js
+"use strict;"
+
+const input = document.querySelector("input");
+input.addEventListener("change", updateValue);
+input.addEventListener("mousemove", updateValue);
+
+function updateValue() {
+    var percent = document.querySelector("span");
+    // console.log(this.value);
+    // console.log(percent.textContent);
+    percent.textContent = this.value;
+    document.documentElement.style.setProperty('--light', this.value/100);
+    document.documentElement.style.setProperty('--glow', this.value/5 + 'px');
+};
